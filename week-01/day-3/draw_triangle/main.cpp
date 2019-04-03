f2f6b1fb1548968386eae3a5953d2f9265d843b6
dezur <petrenyi@gmail.com>
2019-04-03 17:22:13 +0200

    More exercises completed

---
+++ week-01/day-3/draw_triangle/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main(int argc, char *args[]) {
+
+    // Write a program that reads a number from the standard input, then draws a
+    // triangle like this:
+    //
+    // *
+    // **
+    // ***
+    // ****
+    //
+    // The triangle should have as many lines as the number was
+    int number;
+
+    std::cout << "Enter number:";
+    std::cin >> number;
+
+    for (int i = 0; i < number; i++) {
+        for (int j = 0; j < i + 1; j++){
+            std::cout << "*";
+        }
+        std::cout << "\n";
+    }
+
+    return 0;
+}
