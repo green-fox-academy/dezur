6b5aac78c9eb1461c06b6767f9b0fe50a5e427ab
dezur <petrenyi@gmail.com>
2019-04-03 12:58:11 +0200

    First 6 exercises completed

---
+++ week-01/day-3/hello_others/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+
+    // Greet 3 of your classmates with this program, in three separate lines
+    // like:
+    //
+    // Hello, Esther!
+    // Hello, Mary!
+    // Hello, Joe!
+
+    std::cout << "Hello, Esther!\n";
+    std::cout << "Hello, Mary!\n";
+    std::cout << "Hello, Joe!\n";
+
+    return 0;
+}
