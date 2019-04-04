b3422db2a775f73b2df20c6098d4927de6d27367
dezur <petrenyi@gmail.com>
2019-04-04 11:26:50 +0200

    Doubling works!

--- week-01/day-3/draw_triangle/main.cpp
+++ week-01/day-3/draw_triangle/main.cpp
@@ -2,26 +2,26 @@
 
 int main(int argc, char *args[]) {
 
-    // Write a program that reads a number from the standard input, then draws a
-    // triangle like this:
-    //
-    // *
-    // **
-    // ***
-    // ****
-    //
-    // The triangle should have as many lines as the number was
-    int number;
+  // Write a program that reads a number from the standard input, then draws a
+  // triangle like this:
+  //
+  // *
+  // **
+  // ***
+  // ****
+  //
+  // The triangle should have as many lines as the number was
+  int number;
 
-    std::cout << "Enter number: ";
-    std::cin >> number;
+  std::cout << "Enter number: ";
+  std::cin >> number;
 
-    for (int i = 0; i < number; i++) {
-        for (int j = 0; j < i + 1; j++){
-            std::cout << "*";
-        }
-        std::cout << "\n";
+  for (int i = 0; i < number; i++) {
+    for (int j = 0; j < i + 1; j++) {
+      std::cout << "*";
     }
+    std::cout << "\n";
+  }
 
-    return 0;
+  return 0;
 }
