85b1e963cace1fe22ffecebd888ea21605aa0253
dezur <petrenyi@gmail.com>
2019-04-04 15:48:06 +0200

    Swapping array elements works

---
+++ week-01/day-4/swap_elements/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[]) {
+
+  // - Create an array variable named `abc`
+  //   with the following content: `["first", "second", "third"]`
+  // - Swap the first and the third element of `abc`
+
+  std::string abc[3] = {"first", "second", "third"};
+  std::string temp;
+
+  temp = abc[0];
+  abc[0] = abc[2];
+  abc[2] = temp;
+
+  for (int i = 0; i < 3; ++i) {
+    std::cout << abc[i] << " ";
+  }
+
+  return 0;
+}
