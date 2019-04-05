f2578e7e48c1950b9ef5c56809f32522da450bc5
dezur <petrenyi@gmail.com>
2019-04-05 14:20:07 +0200

    Work in progress...

---
+++ week-01/day-3/draw_diagonal/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main(int argc, char* args[]) {
+
+  // Write a program that reads a number from the standard input, then draws a
+  // square like this:
+  //
+  //
+  // %%%%%
+  // %%  %
+  // % % %
+  // %  %%
+  // %%%%%
+  //
+  // The square should have as many lines as the number was
+
+  int numberOfRows;
+
+  std::cout << "Enter number of rows:\n";
+  std::cin >> numberOfRows;
+
+  for (int i = 0; i < numberOfRows ; ++i) {
+    std::cout << "%";
+  }
+
+  for (int j = 0; j < ; ++j) {
+    
+  }
+
+  return 0;
+}
