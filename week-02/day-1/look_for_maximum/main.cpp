5eebb59d818b8f25f3fbcedc0eea98c95ba2a931
dezur <petrenyi@gmail.com>
2019-04-08 15:18:43 +0200

    Look for maximum works

---
+++ week-02/day-1/look_for_maximum/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main()
+{
+    // Create a program which first asks for a number
+    // this number indicates how many integers we want to store in an array
+    // and than asks for numbers till the user fills the array
+    // It should print out the biggest number in the given array and the memory address of it
+
+    int size = 0;
+
+    std::cout << "Enter array size: \n";
+    std::cin >> size;
+
+    int array[size];
+    for (int i = 0; i < size; ++i) {
+        std::cout << "Enter " << i + 1 << ". number:\n";
+        std::cin >> array[i];
+    }
+
+    int max = 0;
+    for (int j = 0; j < size; ++j) {
+        if (array[j] > array[max])
+            max = j;
+    }
+
+    std::cout << "The biggest number is " << array[max] << " at address: " << &array[max];
+
+    return 0;
+}
