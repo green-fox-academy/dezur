76b20d6f59d6f13dcf27d7fabc5964530ef93a45
dezur <petrenyi@gmail.com>
2019-04-08 13:11:44 +0200

    Changing variable using a pointer works

---
+++ week-02/day-1/change_value/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main ()
+{
+    // Change the value of the variable called 'temperature' using a pointer
+    // (you have to create the pointer first)
+
+    float temperature = 21.3;
+    float *tempPointer = &temperature;
+
+    *tempPointer = 24.5;
+
+    std::cout << *tempPointer;
+
+    return 0;
+}
