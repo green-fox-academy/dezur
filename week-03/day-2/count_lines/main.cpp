b05bd2f066590880d46c8ed309ab3d12f148b5d8
dezur <petrenyi@gmail.com>
2019-04-16 13:03:37 +0200

    counting lines in a file works

---
+++ week-03/day-2/count_lines/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int countLines(std::string);
+
+int main()
+{
+    // Write a function that takes a filename as string,
+    // then returns the number of lines the file contains.
+    // It should return zero if it can't open the file
+    std::cout << "The file contains " << countLines("../my-file.txt") << " lines.\n";
+    return 0;
+}
+
+int countLines(std::string fileName)
+{
+    int counter = 0;
+    std::ifstream myFile;
+    myFile.open(fileName);
+    std::string text;
+    while (!myFile.eof()) {
+        getline(myFile, text);
+        counter++;
+    }
+    return counter;
+}
