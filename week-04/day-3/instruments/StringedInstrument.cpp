3900f482864ca2bf85b70037d4c831f59a2b7d76
dezur <petrenyi@gmail.com>
2019-04-24 13:11:03 +0200

    created StringedInstrument class

---
+++ week-04/day-3/instruments/StringedInstrument.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Zoli on 2019. 04. 24..
+//
+
+#include "StringedInstrument.h"
---
+++ week-04/day-3/instruments/StringedInstrument.h
@@ -0,0 +1,16 @@
+//
+// Created by Zoli on 2019. 04. 24..
+//
+
+#ifndef INSTRUMENTS_STRINGEDINSTRUMENT_H
+#define INSTRUMENTS_STRINGEDINSTRUMENT_H
+
+
+#include "Instrument.h"
+
+class StringedInstrument : public Instrument {
+
+};
+
+
+#endif //INSTRUMENTS_STRINGEDINSTRUMENT_H
