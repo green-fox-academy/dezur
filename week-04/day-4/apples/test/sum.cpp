140dc88c4d33bad2ba614e9e83d89161af17bd7b
dezur <petrenyi@gmail.com>
2019-04-25 13:26:34 +0200

    testing sum method with negative sum

--- week-04/day-4/apples/test/sum.cpp
+++ week-04/day-4/apples/test/sum.cpp
@@ -40,3 +40,27 @@ TEST(sumTesting, testSumOfVectorElementsWith1Element)
     // Assert
     ASSERT_EQ(result, 1);
 }
+
+TEST(sumTesting, testSumOfVectorElementsWithMultipleElements)
+{
+    // Arrange
+    std::vector<int> listOfIntegers = {1,2,3,4};
+
+    // Act
+    int result = sum(listOfIntegers);
+
+    // Assert
+    ASSERT_EQ(result, 10);
+}
+
+TEST(sumTesting, testSumOfVectorElementsWithNegativeSum)
+{
+    // Arrange
+    std::vector<int> listOfIntegers = {-1,-2,-3,-4};
+
+    // Act
+    int result = sum(listOfIntegers);
+
+    // Assert
+    ASSERT_EQ(result, -10);
+}
