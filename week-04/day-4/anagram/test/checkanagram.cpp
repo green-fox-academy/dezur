be981319b073e95ecf8d4f5e1fb6342425cb617d
dezur <petrenyi@gmail.com>
2019-04-25 13:42:55 +0200

    created tests for checkanagram function

--- week-04/day-4/anagram/test/checkanagram.cpp
+++ week-04/day-4/anagram/test/checkanagram.cpp
@@ -3,9 +3,32 @@
 //
 
 #include "gtest/gtest.h"
-#include "checkanagram.h";
+#include "checkanagram.h"
 
 TEST(AnagramTesting, testWithAnAnagramWord)
 {
-    
+    // Arrange
+    std::string word1 = "dog";
+    std::string word2 = "god";
+
+    // Act
+    bool result = checkAnagram(word1, word2);
+
+    // Assert
+    ASSERT_EQ(result, true);
+
+}
+
+TEST(AnagramTesting, testWithNonAnagramWord)
+{
+    // Arrange
+    std::string word1 = "dog";
+    std::string word2 = "git";
+
+    // Act
+    bool result = checkAnagram(word1, word2);
+
+    // Assert
+    ASSERT_EQ(result, false);
+
 }
