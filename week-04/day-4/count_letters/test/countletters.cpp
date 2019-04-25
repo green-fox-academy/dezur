0fa21af401d9b3462110d947e7afde21b303cca3
dezur <petrenyi@gmail.com>
2019-04-25 14:56:52 +0200

    testing countLetters function with empty string

--- week-04/day-4/count_letters/test/countletters.cpp
+++ week-04/day-4/count_letters/test/countletters.cpp
@@ -18,3 +18,31 @@ TEST(CountLetterTesting, testWith1Word)
     // Assert
     ASSERT_EQ(result[testChar], 2);
 }
+
+TEST(CountLetterTesting, testWithMultipleWords)
+{
+    // Arrange
+    std::string line = "kiscica kiskutya malacka";
+    char testChar = 'a';
+    std::map<char, int> result;
+
+    // Act
+    result = countLetters(line);
+
+    // Assert
+    ASSERT_EQ(result[testChar], 5);
+}
+
+TEST(CountLetterTesting, testWithEmptyString)
+{
+    // Arrange
+    std::string line = "";
+    char testChar = 'a';
+    std::map<char, int> result;
+
+    // Act
+    result = countLetters(line);
+
+    // Assert
+    ASSERT_EQ(result[testChar], 0);
+}
