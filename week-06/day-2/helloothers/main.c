4641d3dfc7ea5bbec463fd95cf3b49e85f90d7de
dezur <petrenyi@gmail.com>
2019-05-14 11:51:20 +0200

    Hello others completed

---
+++ week-06/day-2/helloothers/main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    // Greet 3 of your classmates with this program, in three separate lines
+    // like:
+    //
+    // Hello, Esther!
+    // Hello, Mary!
+    // Hello, Joe!
+    printf("Hello, Esther!\n");
+    printf("Hello, Mary!\n");
+    printf("Hello, Joe!\n");
+
+    return 0;
+}
