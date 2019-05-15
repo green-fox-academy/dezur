74b2411ba1c9c562a957783d389d6158f76acb3f
dezur <petrenyi@gmail.com>
2019-05-15 13:44:50 +0200

    Points completed

---
+++ week-06/day-3/points/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/*
+Create a point struct which has two float variables: x and y
+Create a function that constructs a point
+It should take it's x and y coordinate as parameter
+Create a function that takes 2 points and returns the distance between them
+Example:
+int main()
+{
+	point_t p1 = create_point(1, 1);
+	point_t p2 = create_point(1, 5.5);
+	float dist = distance(p1, p2);
+	printf("%f", dist);
+	return 0;
+}
+*/
+
+typedef struct
+{
+    float x;
+    float y;
+} point_t;
+
+point_t create_point(float x, float y);
+float distance(point_t, point_t);
+
+int main()
+{
+    point_t p1 = create_point(1, 1);
+    point_t p2 = create_point(1, 5.5);
+    float dist = distance(p1, p2);
+    printf("%f", dist);
+    return 0;
+}
+
+point_t create_point(float x, float y)
+{
+    point_t point;
+
+    point.x = x;
+    point.y = y;
+
+    return point;
+}
+
+float distance(point_t p1, point_t p2)
+{
+    return hypot(p2.x - p1.x, p2.y - p1.y);
+}
