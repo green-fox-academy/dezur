a48d76cbebc22e2b7b9d96e7d11691eba46a5a54
dezur <petrenyi@gmail.com>
2019-05-28 15:00:05 +0200

    LED blinking with interrupt completed

---
+++ week-08/day-2/LED_Blinking_with_interrupt/main.c
@@ -0,0 +1,173 @@
+#include "stm32f7xx.h"
+#include "stm32746g_discovery.h"
+
+static void SystemClock_Config(void);
+
+TIM_HandleTypeDef timer_handle;
+GPIO_InitTypeDef PA15_LED_config;
+
+void init_timer()
+{
+    __HAL_RCC_TIM2_CLK_ENABLE();
+
+    timer_handle.Instance = TIM2;
+    timer_handle.Init.Prescaler = 54000 - 1; 
+    timer_handle.Init.Period = 1000 - 1;
+    timer_handle.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+    timer_handle.Init.CounterMode = TIM_COUNTERMODE_UP;
+
+    /* configuring the timer in PWM mode instead of calling HAL_TIM_Base_Init(&timer_handle) */
+    HAL_TIM_Base_Init(&timer_handle);
+
+    /* assign the highest priority to our interrupt line */
+    HAL_NVIC_SetPriority(TIM2_IRQn, 1, 0);
+
+    /* tell the interrupt handling unit to process our interrupts */
+    HAL_NVIC_EnableIRQ(TIM2_IRQn);
+}
+
+void init_led_pins()
+{
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+
+    PA15_LED_config.Pin = GPIO_PIN_15;
+    PA15_LED_config.Mode = GPIO_MODE_OUTPUT_PP; /* configure as output, in PUSH-PULL mode */
+    PA15_LED_config.Pull = GPIO_NOPULL;
+    PA15_LED_config.Speed = GPIO_SPEED_HIGH;
+
+    HAL_GPIO_Init(GPIOA, &PA15_LED_config);
+}
+
+
+int main(void)
+{
+    HAL_Init();
+
+    /* this function call sets the timers input clock to 108 Mhz (=108000000 Hz) */	
+    SystemClock_Config();
+
+    init_led_pins();
+    init_timer();
+
+    /* starting the timer */
+    HAL_TIM_Base_Start_IT(&timer_handle);
+
+    while (1) {
+	    
+        }
+        
+}
+
+void TIM2_IRQHandler(void)
+{
+    HAL_TIM_IRQHandler(&timer_handle);
+}
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+    if (htim->Instance == TIM2) {
+        /* toggling the LED once in every second */
+        HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_15);
+    }
+}
+
+///////////////////////////// You shouldn't write any code beyond this point //////////////////////////////
+
+static void Error_Handler(void)
+{
+}
+
+static void SystemClock_Config(void)
+{
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+    /**Configure the main internal regulator output voltage */
+    __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+    __HAL_RCC_RNG_CLK_ENABLE();
+
+    /**Initializes the CPU, AHB and APB busses clocks */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+    RCC_OscInitStruct.PLL.PLLM = 8;
+    RCC_OscInitStruct.PLL.PLLN = 216;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+    RCC_OscInitStruct.PLL.PLLQ = 2;
+
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+    {
+        Error_Handler();
+    }
+
+    /**Activate the Over-Drive mode */
+    if (HAL_PWREx_EnableOverDrive() != HAL_OK)
+    {
+        Error_Handler();
+    }
+
+    /**Initializes the CPU, AHB and APB busses clocks */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+void SysTick_Handler(void)
+{
+    HAL_IncTick();
+    HAL_SYSTICK_IRQHandler();
+}
+
+void NMI_Handler(void)
+{
+}
+
+void HardFault_Handler(void)
+{
+    while (1)
+    {
+    }
+}
+
+void MemManage_Handler(void)
+{
+    while (1)
+    {
+    }
+}
+
+void BusFault_Handler(void)
+{
+    while (1)
+    {
+    }
+}
+
+void UsageFault_Handler(void)
+{
+    while (1)
+    {
+    }
+}
+
+void SVC_Handler(void)
+{
+}
+
+void DebugMon_Handler(void)
+{
+}
+
+void PendSV_Handler(void)
+{
+}
